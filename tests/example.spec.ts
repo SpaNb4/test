b7a59c87fb8b3d004a126a17e762a0aae1b61736
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-09-18 23:47:14 +0300

    12

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,17 @@
+// import { test } from '@playwright/test';
+// import { argosScreenshot } from '@argos-ci/playwright';
+
+// test('Main page', async ({ page }) => {
+//   await page.goto('http://localhost:5173');
+
+//   await argosScreenshot(page, 'homepage');
+// });
+
 import { test } from '@playwright/test';
-import { argosScreenshot } from '@argos-ci/playwright';
+import percyScreenshot from '@percy/playwright';
 
 test('Main page', async ({ page }) => {
   await page.goto('http://localhost:5173');
 
-  await argosScreenshot(page, 'homepage');
+  await percyScreenshot(page, 'Main page');
 });
